2d9bc632ff7e6bc3d53ef178e98060a47aefaee6
lizardkingLK <53265303+lizardkingLK@users.noreply.github.com>
2021-01-20 20:07:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SongList from "./components/SongList";
+// import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <SongList />
+    </div>
+  );
+}
